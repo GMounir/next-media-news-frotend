c7f0898e90a6ed95886026728d847791d361a970
Mounir Elgartit <mounirgartit@gmail.com>
2019-10-21 17:44:20 +0100

    add animation fadein class in body tag with favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,12 +11,16 @@ module.exports = {
       dir: 'rtl',
       lang: 'ar',
     },
+    bodyAttrs: {
+      class: 'animated fadeIn'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css'  },
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', integrity:'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr', crossorigin:'anonymous' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
